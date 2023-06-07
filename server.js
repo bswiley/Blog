c3b68ddd9bcabc550afea65ac9d7c90937617114
bswiley <bswiley@gmail.com>
2023-06-07 17:21:41 -0500

    fixed the date

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const handlebars = require('handlebars');
+const { format_time } = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -19,12 +20,11 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({ helpers });
+handlebars.registerHelper('format_time', format_time);
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-Handlebars.registerHelper('format_time', module.exports.format_time);
 app.use(routes);
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
