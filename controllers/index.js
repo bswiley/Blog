f287d214a98e5b910f172b5ec2f90c35b322ee06
bswiley <bswiley@gmail.com>
2023-06-06 22:54:57 -0500

    simplified controllers by deleting api folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
+const apiRoutes = require('./userRoutes.js');
+const siteRoutes = require('./siteRoutes.js');
+
+router.use('/', siteRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
---
+++ controllers/siteRoutes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { Blog, Comment, User } = require('../../models');
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = blogData.map((post) =>
+      post.get({ plain: true })
+    );
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/gallery/:id', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            'id',
+            'title',
+            'artist',
+            'exhibition_date',
+            'filename',
+            'description',
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    // Send over the 'loggedIn' session variable to the 'gallery' template
+    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render('login');
+});
+
+module.exports = router;
