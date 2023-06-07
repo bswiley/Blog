9541cee8e3bdcdf46bc9d45b4e3df0ddde21ebc8
bswiley <bswiley@gmail.com>
2023-06-06 22:58:06 -0500

    retargeted models in siteRoutes and userRoutes

--- controllers/siteRoutes.js
+++ controllers/siteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog, Comment, User } = require('../../models');
+const { Blog, Comment, User } = require('../models');
 
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
