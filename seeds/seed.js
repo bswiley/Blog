46b19aa1562329e388a178a867b5d72b330a8d57
bswiley <bswiley@gmail.com>
2023-06-07 01:15:36 -0500

    fixed the seeding problem

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,36 +1,24 @@
 const sequelize = require('../config/connection');
 const { User, Blog, Comment } = require('../models');
-
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
 const commentData = require('./commentData.json');
-
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
-  for (const blog of blogData) {
-    await Blog.create({
-      ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-
-    for (const comment of commentData) {
-      await Comment.create({
-        ...comment,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
-        blog_id: blog.id,
-      });
-    }
-  }
-
+  const blogs = await Blog.bulkCreate(blogData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const comments = await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
   process.exit(0);
 };
-
 seedDatabase();
 
 
