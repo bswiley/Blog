2ccfebc56a1f41c89bd1a7aee5426b287fa6e18b
bswiley <bswiley@gmail.com>
2023-06-11 16:16:30 -0500

    working on routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 
-
-const dashboardRoutes = require('./dashboard-routes.js');
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-
+router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/home', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
