2579777aea01286364f7ab1c38a8cda232b7d255
bswiley <bswiley@gmail.com>
2023-06-11 19:44:12 -0500

    solved by BCS

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
-module.export = {
-  // the helper method 'format_date' will take in a timestamp and return a string with only the time
-  format_date: (date) => {
+module.exports = {
+  // the helper method 'format_date' will take in a timestamp and return a string with only the date 
+    format_date: (date) => {
     console.log (date)
     // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
     return date.toDateString();
