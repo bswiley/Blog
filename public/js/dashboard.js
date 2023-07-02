175425d6d563fc3147ce5fc74ca78198e0448e23
bswiley <bswiley@gmail.com>
2023-07-01 23:13:10 -0500

    fixed buttons and related javascript & css

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,44 +1,24 @@
 // need to have listeners for "update","delete", and maybe "add" functions.  
 //Actually, these will be user routes so maybe not. 
 buttonSets.forEach((buttonSet) => {
-    const viewButton = buttonSet.querySelector('.view');
     const updateButton = buttonSet.querySelector('.update');
     const deleteButton = buttonSet.querySelector('.delete');
   
-    viewButton.addEventListener('click', () => {
-      // Retrieve information from the card associated with the clicked button
-      const cardTitle = buttonSet.querySelector('.card-title').textContent;
-      const cardUser = buttonSet.querySelector('.card-user').textContent;
-      const cardBody = buttonSet.querySelector('.card-body').textContent;
-  
-      // Perform a GET API request with the retrieved information
-      fetch('your-view-api-endpoint', {
-        method: 'GET',
-        // Include any necessary headers or request parameters
-      })
-        .then((response) => {
-          // Handle the response from the GET API request
-          // Add your desired functionality here
-        })
-        .catch((error) => {
-          // Handle any errors that occur during the API request
-        });
-    });
+    
   
     updateButton.addEventListener('click', () => {
       // Retrieve information from the card associated with the clicked button
       const cardTitle = buttonSet.querySelector('.card-title').textContent;
       const cardUser = buttonSet.querySelector('.card-user').textContent;
       const cardBody = buttonSet.querySelector('.card-body').textContent;
+      const cardId = buttonSet.querySelector('.card-id').textContent;
   
       // Perform a POST API request with the retrieved information
-      fetch('your-update-api-endpoint', {
+      fetch('/api/update', {
         method: 'POST',
-        // Include any necessary headers or request parameters
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          title: cardTitle,
-          user: cardUser,
-          body: cardBody,
+          id: cardId,
         }),
       })
         .then((response) => {
